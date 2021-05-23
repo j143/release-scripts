9a26ac0ce63a5285f8574c492c829867d3284fe3
Janardhan Pulivarthi <janardhan@apache.org>
2021-05-23 16:33:09 +0530

    release-build publish-snapshot is working fine

--- release-build.sh
+++ release-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-cat << EOF
+exit_with_usage() {
+
+  cat << EOF
 usage: release-details.sh <package|docs>
 Create build deliverables from a commit
 Top level targets are
@@ -15,94 +17,47 @@ ASF_USERNAME - Username of ASF committer
 ASF_PASSWORD - Password of ASF committer account
 GPG_KEY - GPG key used to sign release artifacts
 GPG_PASSPHRASE - Passphrase for GPG key
-
 EOF
+  exit 1
+}
 
 if [ $# -eq 0 ]; then
   echo "usage: release-details.sh <package|docs>"
 fi
 
-function error {
+error() {
   echo "$*"
   exit 1
 }
+
+if [ $# -eq 0 ]; then
+  exit_with_usage
+fi
+
 
 if [[ $@ == *"help"* ]]; then
   echo "help is on its way."
 fi
 
-cat <<EOF
-================
-Release details:
-BRANCH:     $GIT_BRANCH
-VERSION:    $RELEASE_VERSION
-TAG:        $RELEASE_TAG
-NEXT:       $NEXT_VERSION
-ASF USER:   $ASF_USERNAME
-GPG KEY:    $GPG_KEY
-FULL NAME:  $GIT_NAME
-E-MAIL:     $GIT_EMAIL
-================
-EOF
-
-# cat pom.xml | grep -e '<version>.*</version>' |  head -n 2 | tail -n 1 | cut -d '>' -f2 | cut -d '<' -f1
-
-# Find out the latest branch
-# git ls-remote --head origin master | grep -v refs/head/master | awk '{print $2}' | head -n 1 | cut -d/ -f3
-# master
-
-function init_java {
-    if [ -z "$JAVA_HOME"]; then
-      error "JAVA_HOME is not set."
-    fi
-    JAVA_VERSION=$("${JAVA_HOME}"/bin/javac -version 2>&1 | cut -d " " -f2)
-    export JAVA_VERSION
-    echo "$JAVA_VERSION"
-}
-
-init_java
-
-mvn deploy -DskipTests \
-  -DaltSnapshotDeploymentRepository=sonatype-nexus-snapshots::default::http://localhost:8081/repository/sonatype-nexus-snapshot
-
-
-# check maven version
-mvn -version 2>&1 | grep 'Maven home' | awk '{print $NF}'
-# /usr/local/apache-maven-3.5.4
-
-# Build maven package
-
-git_hash=`git rev-parse --short HEAD`
-
-export GIT_HASH=$git_hash
-
-echo "Checked out SystemDS git has $git_hash"
-# Checked out SystemDS git has bef8ec754
-
-cd ..
-
-tar cvzf systemds-2.1.0.tgz --exclude systemds-2.1.0/.git systemds-2.1.0
-
-shasum -a 512 systemds-2.1.0.tgz > systemds-2.1.0.tgz.sha512
-
-rm -rf systemds-2.1.0
-
-# making binary release
-
-cp -r systemds systemds-2.1.0-bin
-cd systemds-2.1.0-bin
-
-echo "Creating distribution: systemds-2.1.0-bin"
 
 # Build docs (production)
+if [[ "$1" == "docs" ]]; then
+  cd systemds
+  echo "Building SystemDS docs"
 
-cd systemds
-echo "Building SystemDS docs"
+  cd docs
 
-cd docs
+  bundle install
+  PRODUCTION=1 RELEASE_VERSION="2.1.0" bundle exec jekyll build
+fi
 
-bundle install
-PRODUCTION=1 RELEASE_VERSION="2.1.0" bundle exec jekyll build
+
+if [[ "$1" == "publish-snapshot" ]]; then
+  cd systemds
+  mvn deploy -DskipTests \
+    -DaltSnapshotDeploymentRepository=sonatype-nexus-snapshots::default::http://localhost:8081/repository/sonatype-nexus-snapshot
+
+fi
 
 # if [[ -z "$GPG_KEY" ]]; then
 #   echo "The environment variable $GPG_KEY is not set."
