9e969fd20ff5681d8168bc1df607e499b1537121
Janardhan Pulivarthi <j143@protonmail.com>
2021-06-06 00:11:28 +0530

    Update release-utils.sh

--- release-utils.sh
+++ release-utils.sh
@@ -160,8 +160,10 @@ E-MAIL:     $GIT_EMAIL
 ================
 EOF
 
-  read -p "Is this info correct [Y/n]? " ANSWER
-  if [ "$ANSWER" != "Y" ]; then
+#   read -p "Is this info correct [Y/n]? " ANSWER
+  CORRECT_RELEASE_INFO=$(read_config "Is the release info correct (1 for Yes, 0 for No) ?" "$CORRECT_RELEASE_INFO")
+  
+  if ! CORRECT_RELEASE_INFO; then
     echo "Exiting."
     exit 1
   fi
